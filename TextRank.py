6df008f0f7c44010d67d50d23e24d79a035b5690
AlexWills37 <alexanderwills37@gmail.com>
2022-11-30 23:19:17 -0500

    created skeleton for TextRank

---
+++ TextRank.py
@@ -0,0 +1,23 @@
+'''
+Implementation of the TextRank algorithm
+'''
+
+
+
+def sentenceSimilarity(sentence1, sentence2) -> float:
+    '''
+    Calculates the similarity between two sentences.
+    '''
+    return
+
+
+def updateSentenceGraph():
+    '''
+    Performs one iteration of the TextRank algorithm to update every sentence's relevancy score.
+    '''
+
+def textRank():
+    '''
+    Repeatedly updates sentence relevancy scores until they converge and do not change by much.
+    '''
+    return
