5cef3b2fcad56406e62bfc424109acb1a18bf7f1
AlexWills37 <alexanderwills37@gmail.com>
2022-12-04 13:20:20 -0500

    added cosine similarity

--- Test.py
+++ Test.py
@@ -3,16 +3,19 @@ import re
 from nltk import sent_tokenize
 
 def main():
-    print()
+    # print()
 
-    with open( os.path.join(os.path.dirname(__file__), "Corpus/Winter.txt"), mode="r", encoding="utf-8") as file:
-        rawText = file.read()
+    # with open( os.path.join(os.path.dirname(__file__), "Corpus/Winter.txt"), mode="r", encoding="utf-8") as file:
+    #     rawText = file.read()
 
-        lines = rawText.split("\n")
-        sentences = sent_tokenize(rawText)
+    #     lines = rawText.split("\n")
+    #     sentences = sent_tokenize(rawText)
 
-        for sent in sentences:
-            print(sent.replace("\n", ""))
+    #     for sent in sentences:
+    #         print(sent.replace("\n", ""))
+
+
+    print( [1, 2, 3, 4] * 0.5)
 
         
 
--- TextRank.py
+++ TextRank.py
@@ -21,55 +21,53 @@ def sentenceSimilarity(sentence1, sentence2, embeddings_dict) -> float:
         # Turn each word into a vector with GloVe, then average all of them
     wordsSentence1 = pp.tokenizeSentence(sentence1)
     wordsSentence2 = pp.tokenizeSentence(sentence2)
-    
 
-    embeddingsCount = 0
-    unknownWords = []
-    combinedEmbedding = []
-    for word in wordsSentence1:
+    # Get average for sentence 1
+    avgSentence1 = calcAverageEmbedding(wordsSentence1, embeddings_dict)
 
-        # If word has an embedding, add it to the sentence's average embedding
-        if word in embeddings_dict:
-            newEmbedding = embeddings_dict[word]
-
-            if len(combinedEmbedding) == 0:
-                combinedEmbedding = newEmbedding
-            else:
-                np.add(combinedEmbedding, newEmbedding)
-
-            embeddingsCount += 1
-        else:
-            unknownWords.append(word)
-
-    embeddingsCount = 0
-    unknownWords = []
-    combinedEmbedding = []
-    for word in wordsSentence2:
-
-        # If word has an embedding, add it to the sentence's average embedding
-        if word in embeddings_dict:
-            newEmbedding = embeddings_dict[word]
-
-            if len(combinedEmbedding) == 0:
-                combinedEmbedding = newEmbedding
-            else:
-                np.add(combinedEmbedding, newEmbedding)
-
-            embeddingsCount += 1
-        else:
-            unknownWords.append(word)
-
-    # Get average
-    avgSentence2 = combinedEmbedding * (1 / embeddingsCount)
+    # Get average for sentence 2
+    avgSentence2 = calcAverageEmbedding(wordsSentence2, embeddings_dict)
   
-
-
-
     # Find the cosine similarity between averages
-    spatial.distance.cosine()
+    return np.dot(avgSentence1, avgSentence2) / (np.linalg.norm(avgSentence1) * np.linalg.norm(avgSentence2))
 
-    return 
 
+def calcAverageEmbedding(sentence: list, embeddings_dict):
+    '''
+    Calculates the average embedding of a sentence.
+
+    Skips over words that are not in the embeddings_dict
+
+    @param sentnece - a list of preprocessed tokens representing the sentence as words
+    @param embeddings_dict - the dictionary that maps words to their embeddings
+
+    @return - the average embeddings, calculated from the embedding of each word.
+    '''
+
+    # Add up all embeddings
+    embeddingsCount = 0
+    unknownWords = []
+    combinedEmbedding = []
+    for word in sentence:
+
+        # If word has an embedding, add it to the sentence's average embedding
+        if word in embeddings_dict:
+            newEmbedding = embeddings_dict[word]
+
+            # For the first embedding, set the total (so that combinedEmbedding is the right size)
+            if len(combinedEmbedding) == 0:
+                combinedEmbedding = newEmbedding
+            else:
+                combinedEmbedding = np.add(combinedEmbedding, newEmbedding)
+
+            embeddingsCount += 1
+
+        # Skip over words that do not have embeddings
+        else:
+            unknownWords.append(word)
+
+    # Return average
+    return combinedEmbedding * (1.0 / embeddingsCount)
 
 def updateSentenceGraph():
     '''
@@ -84,7 +82,18 @@ def textRank():
 
 
 def buildSimilarityMatrix(sentences):
+    '''
+    Builds a similarity matrix between every sentence.
+    This matrix represents the edges between sentences in a fully connected graph, where each vertex is a sentence, and each edge
+    is the similarity between two sentences, from 0 to 1.
+
+    @param sentences - a list of sentences. Each sentence is a list whose first element is a string containing the sentence
+
+    @return - a similarity matrix for all of the sentences. To get the similarity/edge between two sentences,
+    index into this matrix with the indices of the sentences
     
+    Example: to get the similarity between sentence 5 and 7, access matrix[5][7]. This is also the same as matrix[7][5].
+    '''
 
     # Create word embedding dictionary
     in_file = 'data/glove.6B.50d.txt'
@@ -104,6 +113,7 @@ def buildSimilarityMatrix(sentences):
     for i in range(len(sentences)):
         matrix.append([0] * len(sentences))
 
+    # Calculate the similarity for every combination of sentences
     for row in range(len(sentences)):
 
         sentence1 = sentences[row][0]
@@ -130,22 +140,18 @@ def buildSimilarityMatrix(sentences):
             matrix[row][col] = similarity
             matrix[col][row] = similarity
 
-
-
     return matrix
 
 def main():
 
 
-
-    
-
-
     sentences = {
         0: ["Apples are in Stardew valley and sell for gold.", 0.1],
         1: ["Stardew valley is a game.", 0.1],
-        2: ["Cats are cool and cute", 0.1],
-        3: ["Stardew Valley has trees and stuff.", 0.1]
+        2: ["Cats!", 0.1],
+        3: ["Apples are in Stardew valley and sell for gold and cute cats.", 0.1],
+        4: ["Without the energy of the sun, scientists cannot find the sun.", 0.1],
+        5: ["Stardew valley is a farming and life simulator with town NPCs and lots of interactions.", 0.1]
     }
 
     similarityMatrix = buildSimilarityMatrix(sentences)
